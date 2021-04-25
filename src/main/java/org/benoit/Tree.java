7e8dbc1e7624651fc9a9a399c8a13cf966247d88
Benoit Pasquereau <bpasquereau@gmail.com>
2021-04-25 17:10:28 +0100

    Simplified some types

--- src/main/java/org/benoit/Tree.java
+++ src/main/java/org/benoit/Tree.java
@@ -2,9 +2,9 @@ package org.benoit;
 
 import java.util.AbstractMap;
 
-// Java 15
+// Java 16/ Java 15 with preview enabled
 public class Tree<T> {
-    // The class should be made a singleton but doesn't seem to work with the type parameters
+    // The Tree class should be made a singleton but doesn't seem to work with the type parameters
     public interface INode<T> {
     }
     public record Branch<T>(T value, INode<T> left, INode<T> right) implements INode<T> {
@@ -21,15 +21,15 @@ public class Tree<T> {
     
     AbstractMap.SimpleEntry<Integer,INode<AbstractMap.SimpleEntry<Integer,T>>>
         addId(INode<T> node, Integer start) {
-        INode<AbstractMap.SimpleEntry<Integer,T>> l = new Leaf<AbstractMap.SimpleEntry<Integer,T>>();
+        INode<AbstractMap.SimpleEntry<Integer,T>> l = new Leaf<>();
         var r = new AbstractMap.SimpleEntry<>(start, l);
         // Waiting for switch on types (https://openjdk.java.net/jeps/8213076)
         // but still nice to use JEP 394 (pattern matching for instanceof)
         if (node instanceof Branch<T> b) {
             var newLeft = addId(b.left(), start);
             var newRight = addId(b.right(), newLeft.getKey());
-            var newBranch = new Branch<AbstractMap.SimpleEntry<Integer,T>>(
-                    new AbstractMap.SimpleEntry<Integer,T>(newRight.getKey(), b.value()),
+            var newBranch = new Branch<>(
+                    new AbstractMap.SimpleEntry<>(newRight.getKey(), b.value()),
                     newLeft.getValue(),
                     newRight.getValue()
             );
